b26f7df9cdf432f483cb557dca487b709ccf8ae1
leocarv <leopsyrj@gmail.com>
2012-11-07 13:52:48 -0200

    Exercício 2

---
+++ ex2.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+# Um comentáo serve para vocêer melhor o seu programa
+# tudo que vier depois do # égnorado pelo pyhton
+
+print "I could have code like this." #esse comentario égnorado
+
+# You can also use a comment do "disable" or coment out a piece of code
+#print "This won't run."
+
+print "This will run."
