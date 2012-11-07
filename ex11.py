ae99cc1647d42f354e43b69125b7237d6e0c1f92
leocarv <leopsyrj@gmail.com>
2012-11-07 17:00:50 -0200

    Exercicio 11 (ajustado)

--- ex11.py
+++ ex11.py
@@ -1,8 +1,10 @@
 ﻿# -*- coding: utf-8 -*-
-#print "How old are you?",
-age = raw_input("How old are you?")
-height = input("How tall are you?")
-weight = raw_input("How much do you weight?")
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
 
 print "So, you're %r old, %r tall and %r heavy." % (
-	age, height, weight)
+    age, height, weight)
