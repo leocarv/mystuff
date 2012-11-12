c97d8e95d98867b0f18de9a340618174d8801292
leocarv <leopsyrj@gmail.com>
2012-11-12 15:25:04 -0200

    Exercicio 36

---
+++ ex36.py
@@ -0,0 +1,31 @@
+﻿# -*- coding: utf-8 -*-
+from sys import exit
+
+def dead(why):
+    print why, "Good job!"
+    exit(0)
+    
+def start():
+    print "Inicio do jogo."
+    print "Escolhe esquerda ou direita?."
+    
+    escolha = raw_input("> ")
+    
+    if escolha == "esquerda":
+        sala_esquerda()
+    elif escolha == "direita":
+        sala_direita()
+    else:
+        dead("O jogo acabou!")
+
+def sala_esquerda():
+    print "Bem vindo a sala da esquerda"
+    print "Deseja ir para onde?"
+
+
+def sala_direita():
+    print "Bem vindo a sala da direita"
+    print "Deseja ir para onde?"
+
+        
+start()
