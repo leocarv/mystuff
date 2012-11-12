b97422274190b867702e6d09523ae32f93e69bda
leocarv <leopsyrj@gmail.com>
2012-11-12 11:56:21 -0200

    Exercicio 33

---
+++ ex33.py
@@ -0,0 +1,16 @@
+﻿# -*- coding: utf-8 -*-
+numbers = []
+
+def functionWhile(final, incr):
+    for i in range(0, final, incr):
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i += incr
+        print "Numbers now: ", numbers    
+        print "At the bottom i is %d" % i
+
+functionWhile(6, 2)
+print "The numbers:"
+for num in numbers:
+    print num
